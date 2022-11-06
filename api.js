b837b5fcf2be5362bb23c30e7410fc5ec7ba8ff6
hulyio <00039718@uca.edu.sv>
2022-11-06 14:54:34 -0600

    added get by id, post user, fixed pg password

--- api.js
+++ api.js
@@ -19,3 +19,26 @@ app.get('/users', (req, res)=>{
     });
     client.end;
 })
+
+app.get('/users/:id', (req, res)=>{
+    client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+app.post('/users', (req, res)=> {
+    const user = req.body;
+    let insertQuery =   `insert into users(id, firstname, lastname, location) 
+                        values(${user.id}, '${user.firstname}', '${user.lastname}', '${user.location}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
